79d0cfa03d7b275da250c6696d4612d51a52ff30
Findus23 <l.winkler23@me.com>
2013-12-31 15:52:29 +0100

    Beginn eines Menüs

---
+++ menue.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css" />
+
+</head>
+<body>
+<nav>
+<a href="test">Veranstaltungen</a>
+<a href="test">test</a>
+<a href="test">test</a>
+<a href="test">test</a>
+
+</nav>
+<?php
+?>
+</body>
+</html>
