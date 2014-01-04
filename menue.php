2e76bdba0457d3626b7a0e8ec59b20d73a0591c5
Findus23 <l.winkler23@me.com>
2014-01-04 15:52:17 +0100

    Menü

--- menue.php
+++ menue.php
@@ -1,15 +1,22 @@
 <html>
 <head>
 <link rel="stylesheet" href="style.css" />
-
+<script type="text/javascript">
+function Fenster_klein (Adresse) {
+  MeinFenster = window.open(Adresse, "Zweitfenster", "width=300,height=200,left=100,top=200");
+  MeinFenster.focus();
+}
+function Fenster_breit (Adresse) {
+  MeinFenster = window.open(Adresse, "Zweitfenster", "width=500,height=400,left=100,top=200");
+  MeinFenster.focus();
+}
+</script>
 </head>
 <body>
+Du bist nicht angemeldet - <a href="./login.php" onclick="Fenster_klein(this.href, 300); return false"> melde dich</a> an oder <a href="./registrieren.php" onclick="Fenster_breit(this.href, 600); return false">registriere dich zum ersten mal</a>
 <nav>
-<a href="test">Veranstaltungen</a>
-<a href="test">test</a>
-<a href="test">test</a>
-<a href="test">test</a>
-
+<a href="./veranstaltungen.php">Veranstaltungen</a>
+<a href="./orte.php">Orte</a>
 </nav>
 <?php
 ?>
